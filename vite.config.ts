971a0c31a2e3a8e9b0570a860ace8b2ee5a2404a
panamini <panamini@tutamail.com>
2026-03-23 20:36:21 +0100

    add custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  
+    base: "/dasti-theme-lab/",
   plugins: [react()],
   resolve: {
     alias: {
